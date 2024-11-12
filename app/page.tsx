fa91607e3d6323870bf6b4ff7b116ebb984c6caa
Diego <dazet@outlook.cl>
2024-11-12 11:32:17 -0300

    Add image component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+//components
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
+import {Image} from "@nextui-org/image";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
@@ -11,6 +13,13 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
+        <div className="flex justify-center">
+          <Image
+            width={300}
+            alt="NextUI hero Image"
+            src="https://nextui.org/images/hero-card-complete.jpeg"
+          />
+        </div>
         <span className={title()}>Make&nbsp;</span>
         <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
         <br />
